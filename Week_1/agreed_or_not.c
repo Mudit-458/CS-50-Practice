25d6dfdace3cbf10265cb24cef4ace673beab921
Mudit-458 <mudit.dixit458@gmail.com>
2026-06-21 08:00:58 +0000

    Little Clean-up

--- Week_1/agreed_or_not.c
+++ Week_1/agreed_or_not.c
@@ -3,19 +3,13 @@
 
 int main(void)
 {
-    char c = get_char("Do you agree?\n");
-    
+    char c = get_char("Do you agree?\n");    
     if (c == 'y' || c == 'Y')
 {
     printf("Agreed.\n");
 }
-
 else
-
 {
     printf("Not agreed.\n");
     }
-
-
-
 }
