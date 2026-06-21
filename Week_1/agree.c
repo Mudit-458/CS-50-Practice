00422346648d41ef97b7759577bac3363e889f5c
Mudit-458 <mudit.dixit458@gmail.com>
2026-06-21 07:45:52 +0000

    Cleanup root binaries

---
+++ Week_1/agree.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<cs50.h> 
+
+int main(void)
+{
+    char c = get_char("Do you agree?\n");
+    
+    if (c == 'y' || c == 'Y')
+{
+    printf("Agreed.\n");
+}
+
+else
+
+{
+    printf("Not agreed.\n");
+    }
+
+
+
+}
